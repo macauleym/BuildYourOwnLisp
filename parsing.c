d2bb77f63bf81233bf2fe92d49dbd61f85659c64
Michael Macauley <macaulm@yahoo.com>
2023-02-24 00:25:57 -0500

    Finished up following along with chapter 8 (Error Handling), and most of the bonus marks.

--- parsing.c
+++ parsing.c
@@ -38,6 +38,73 @@ void add_history(char* unused) { }
 #define MIN "min"
 #define MAX "max"
 
+// Declare a struct to use as a result.
+typedef struct {
+    union {
+        struct { union { long num; double numd; }; } right;
+        struct { int err;  } left ;
+    };
+    int type;
+} result;
+
+// Enum to give result values names.
+enum RESULT_TYPE { RESULT_RIGHT, RESULT_LEFT };
+
+// Enum to give names to error type values.
+enum RERR_TYPE { RERR_DIV_ZERO, RERR_BAD_OP, RERR_BAD_NUM };
+
+// Functions to construct the valid (right) result
+// and the error (left) result.
+result result_right(long x) {
+    result r;
+    r.type = RESULT_RIGHT;
+    r.right.num  = x;
+
+    return r;
+}
+
+result result_left(int x) {
+    result r;
+    r.type = RESULT_LEFT;
+    r.left.err  = x;
+
+    return r;
+}
+
+char* build_err_msg(char* msg) {
+    return strcat("ERROR :: ", msg);
+}
+
+void result_print(result r) {
+    switch (r.type) {
+        // If we get a valid result num, just print it and break.
+        case RESULT_RIGHT:
+            printf("%li", r.right.num);
+        break;
+
+        // If we get an error, we need to also check what type
+        // of error we got.
+        case RESULT_LEFT:
+            if (r.left.err == RERR_DIV_ZERO) {
+                printf("ERROR :: Division by Zero!");
+            }
+
+            if (r.left.err == RERR_BAD_OP) {
+                printf("ERROR :: Invalid Operator!");
+            }
+
+            if (r.left.err == RERR_BAD_NUM) {
+                printf("ERROR :: Invalid Number!");
+            }
+        break;
+    }
+}
+
+void result_println(result r) {
+    result_print(r);
+    putchar('\n');
+}
+
 int number_of_nodes(mpc_ast_t* pTree) {
     // Base case, when there are no children.
     if (pTree->children_num == 0) { return 1; }
@@ -57,35 +124,44 @@ int number_of_nodes(mpc_ast_t* pTree) {
 }
 
 // Use operator string to see which operation to perform.
-long evaluate_op(long x, char* op, long y) {
-    printf("evaluating op as: %li %s %li\n", x, op, y);
-    long opResult = 0;
-    if (strcmp(op, ADD) == 0) { opResult = x + y;         }
-    if (strcmp(op, SUB) == 0) { opResult = x - y;         }
-    if (strcmp(op, MUL) == 0) { opResult = x * y;         }
-    if (strcmp(op, DIV) == 0) { opResult = x / y;         }
-    if (strcmp(op, MOD) == 0) { opResult = x % y;         }
-    if (strcmp(op, POW) == 0) { opResult = powl(x, y);    }
-    if (strcmp(op, MIN) == 0) { opResult = x < y ? x : y; }
-    if (strcmp(op, MAX) == 0) { opResult = x > y ? x : y; }
+result evaluate_op(result leftOperand, char* op, result rightOperand) {
+    long x = leftOperand.right.num;
+    long y = rightOperand.right.num;
+
+    result opResult = result_left(RERR_BAD_OP);
+
+    // Operators.
+    if (strcmp(op, ADD) == 0) { opResult = result_right(x + y);         }
+    if (strcmp(op, SUB) == 0) { opResult = result_right(x - y);         }
+    if (strcmp(op, MUL) == 0) { opResult = result_right(x * y);         }
+    if (strcmp(op, DIV) == 0) { opResult = y == 0
+                                         ? result_left(RERR_DIV_ZERO)
+                                         : result_right(x / y);         }
+    if (strcmp(op, MOD) == 0) { opResult = result_right(x % y);         }
+    if (strcmp(op, POW) == 0) { opResult = result_right(powl(x, y));    }
+
+
+    // Builtin functions.
+    if (strcmp(op, MIN) == 0) { opResult = result_right(x < y ? x : y); }
+    if (strcmp(op, MAX) == 0) { opResult = result_right(x > y ? x : y); }
 
     return opResult;
 }
 
-long evaluate_ast(mpc_ast_t* pTree) {
+result evaluate_ast(mpc_ast_t* pTree) {
     // Base case, if tag is 'number' return it.
     if (strstr(pTree->tag, "number")) {
-        long num = atoi(pTree->contents);
-        // atoi = ascii(?) to integer
-        printf("hit a number! returning %li\n", num);
-        return num;
+        // Check for conversion errors.
+        errno = 0;
+        long num = strtol(pTree->contents, NULL, 10);
+        return errno != ERANGE
+            ? result_right(num)
+            : result_left(RERR_BAD_NUM);
     }
 
     if (strstr(pTree->tag, "expr")) {
         printf("node is an expr!\n");
     }
-
-    // Process builtin function
 
     // We've got a parent branch, determine the operator.
     // We add to the offset if the current contents is a '(' character.
@@ -94,28 +170,20 @@ long evaluate_ast(mpc_ast_t* pTree) {
     char* op = "";
     for (;;) {
         op = pTree->children[next]->contents;
-        printf("value of op is: (->%s<-)\n", op);
         if (strcmp(op, "(") == 0 || strcmp(op, "") == 0) {
             next++;
-            printf("op is not valid, incrementing next\n");
         } else {
-            printf("op is valid, breaking\n");
             break;
         }
     }
 
-    printf("next value: %li\n", next);
-
     // Store the next child.
-    long x = evaluate_ast(pTree->children[next+1]);
-    printf("value of 'x': %li\n", x);
+    result x = evaluate_ast(pTree->children[next+1]);
 
     // Iterate the remaining children.
     int i = next+2;
     int hasOperated = 0;
-    printf("iterating children...\n");
     while (strstr(pTree->children[i]->tag, "expr")) {
-        printf("Evaluating child #%li\n", i);
         x = evaluate_op(x, op, evaluate_ast(pTree->children[i]));
         i++;
         hasOperated = 1;
@@ -123,11 +191,9 @@ long evaluate_ast(mpc_ast_t* pTree) {
 
     // If we were given a '-' with only 1 argument, negate the argument.
     if (strcmp(op, SUB) == 0 && hasOperated == 0) {
-        printf("a '-' was given a single argument! returning: %li\n", -x);
-        return -x;
+        return result_right(-x.right.num);
     }
 
-    printf("no more processing for current tree! returning: %li\n", x);
     return x;
 }
 
@@ -179,13 +245,13 @@ int main(int argc, char** argv) {
         add_history(input);
 
         // Attempt to parse the input against the grammar we've created.
-        mpc_result_t result;
-        if (mpc_parse("<stdin>", input, Lispish, &result)) {
+        mpc_result_t res;
+        if (mpc_parse("<stdin>", input, Lispish, &res)) {
             // Success!
             // Load the AST from the output.
-            mpc_ast_t* ast = result.output;
-            long evaluated = evaluate_ast(ast);
-            printf("%li\n", evaluated);
+            mpc_ast_t* ast = res.output;
+            result evaluated = evaluate_ast(ast);
+            result_println(evaluated);
 
             // Print the AST (Abstract Syntax Tree).
             mpc_ast_print(ast);
@@ -194,8 +260,8 @@ int main(int argc, char** argv) {
             mpc_ast_delete(ast);
         } else {
             // Error! Failed to parse the input.
-            mpc_err_print(result.error);
-            mpc_err_delete(result.error);
+            mpc_err_print(res.error);
+            mpc_err_delete(res.error);
         }
 
         // malloc() was called, so we need to free().
